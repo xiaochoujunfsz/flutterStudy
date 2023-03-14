a5bd818c04604848f4d22379cfb5dd75bbd58a48
fushaozhen <541508448@qq.com>
2023-03-14 17:07:46 +0800

    掘金flutter学习-秒表

---
+++ gsy_app_test/lib/env/env_config.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env_config.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+EnvConfig _$EnvConfigFromJson(Map<String, dynamic> json) => EnvConfig(
+      env: json['env'] as String?,
+      debug: json['debug'] as bool?,
+    );
---
+++ juejingstudy/Stopwatch/android/app/src/main/kotlin/com/xcj/stopwatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xcj.stopwatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
