9427cd61bec3851b30c6ab6196b406a6bc3beeaf
fushaozhen <541508448@qq.com>
2023-07-27 15:11:30 +0800

    练习9

---
+++ juejingstudy/ilayout_study/lib/09/02/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Align(
+    alignment: Alignment.center,
+    child: SizedBox(
+      width: 80,
+      height: 40,
+      child: ColoredBox(
+        color: Colors.blue,
+      ),
+    ),
+  ));
+}
